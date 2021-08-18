d6566a50e71dfbe8aa41036229e2deeb4a17a6c9
Abhay0809 <48680107+Abhay0809@users.noreply.github.com>
2021-08-18 09:07:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NPM Business Card
---
+++ index.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+// Questions after the card 
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:abhayanandkk23@gmail.com");
+                    console.log("\nDone, see you soon.\n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Good Bye!\n");
+                }
+            }
+        ]
+    }
+];
+
+// Data for the card
+const data = {
+    name: chalk.bold.green("                                            ") + chalk.bold.hex('#ff00ff').underline("Abhay Anand"),
+    subhead: chalk.bold.white("abhayanand $ ") + chalk.bold.green(" cd aboutabhay"),
+    profession: `${chalk.white("B.Tech Student in")} ${chalk.hex("#fccc63").bold("Information Technology")}`,
+    instagram: chalk.gray("https://www.instagram.com/") + chalk.hex('#cd486b').bold("hashtag_abhay"),
+    twitter: chalk.gray("https://twitter.com/") + chalk.hex('#00acee').bold("codo__sapien"),
+    github: chalk.gray("https://github.com/") + chalk.hex('#f5f5f5').bold("Abhay0809"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.cyan.bold("abhay-anand1108"),
+    web: chalk.hex('#FF6666').bold("https://abhay-web-folio.netlify.app/"),
+    npx: chalk.greenBright("npx") + " " + chalk.white.bold("abhayanand"),
+
+    labelProfession: chalk.white.bold(" Profession:"),
+    labelInstagram: chalk.white.bold("  Instagram:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+// Build the card
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.subhead}`,
+        ``,
+        `${data.labelProfession}  ${data.profession}`,
+        `${data.labelInstagram}  ${data.instagram}`,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I like connecting with different people so if you want to say hi, I'll be happy to chat with you!"
+        )}`,
+        `${chalk.italic("The rest of the time I experiment with my code, to bring my ideas to life.")}`
+    ].join("\n"),
+    {
+        margin: 2.5,
+        float: 'center',
+        padding: 2,
+        borderStyle: "double",
+        borderColor: "#50DBB4"
+    }
+);
+
+// Print the card
+console.log(me);
+
+// Optional tip to help users use the links
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "ctrl/cmd + click"
+    )} on the links above`,
+    '',
+].join("\n");
+
+// Show the tip 
+console.log(tip);
+
+// Ask the Inquirer questions. 
+prompt(questions).then(answer => answer.action());
